43d039d9bc5f46f7b8c94a8c8ec1ca29abec6d89
Aymanfox2002 <aymanfox2002.99@gmail.com>
2025-10-16 00:57:16 +0300

    Move Resend initialization inside POST function for better encapsulation

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,9 +1,9 @@
 import { EmailTemplate } from "../../../components/email-tamplate";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const { username, email, message } = await req.json(); // Destructure 'username' directly
     const { data, error } = await resend.emails.send({
